845881dff8ded8c3546030fa177c9aa8116054f5
Dandy <112651184+Dendzy@users.noreply.github.com>
2023-08-05 22:06:42 -0300

    Create Atividade Desenvolvimento XVIII.js

---
+++ Atividade Desenvolvimento XVIII.js
@@ -0,0 +1,12 @@
+var animais = [
+  ['Cavalo', 'Equus ferus', 10],
+  ['Zebra', 'Equus quagga', 12],
+  ['Leão', 'Panthera leo', 8],
+  ['Cachorro', 'Canis lupus familiaris', 5]
+]
+
+// Imprimindo os nomes:
+console.log(animais[0][0]);
+console.log(animais[1][0]);
+console.log(animais[2][0]);
+console.log(animais[3][0]);
